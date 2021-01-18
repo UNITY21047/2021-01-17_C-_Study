514b512628645dc1ee4ceea3a7cc6da3866ad110
Gary Smith <unity21047@gmail.com>
2021-01-18 01:07:32 -0600

    helloworld.cpp:
    Changed syntax of the calss interface for using a button.

--- pt_1/src/helloworld.cpp
+++ pt_1/src/helloworld.cpp
@@ -3,8 +3,7 @@
 #include "helloworld.h"
 #include <iostream>
 
-HelloWorld::HelloWorld()
-: m_button("Hello World")   // creates a new button with label "Hello World".
+HelloWorld::HelloWorld() : m_button("Hello World")   // creates a new button with label "Hello World".
 {
   // Sets the border width of the window.
   set_border_width(10);
